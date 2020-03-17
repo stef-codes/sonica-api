48950adaf6b94b86a50f8e4a026792b2b9cb3b52
Stefanie <stefcodes@gmail.com>
2020-03-17 00:30:57 -0700

    attempt to seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-"artistName":"Jhené Aiko", "collectionName":"Trip", "trackName":"Frequency" "artistViewUrl":"https://music.apple.com/us/artist/jhen%C3%A9-aiko/198431322?uo=4", "collectionViewUrl":"https://music.apple.com/us/album/frequency/1440907359?i=1440908473&uo=4", "trackViewUrl":"https://music.apple.com/us/album/frequency/1440907359?i=1440908473&uo=4", 
-    "previewUrl":"https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview118/v4/a7/2c/a7/a72ca761-d36f-d383-b62c-43f31a6eb7de/mzaf_3908667139953994176.plus.aac.p.m4a", "artworkUrl30":"https://is2-ssl.mzstatic.com/image/thumb/Music128/v4/9c/5a/94/9c5a94ad-b20b-aaf1-9057-e1fa801ed56a/source/30x30bb.jpg", "artworkUrl60":"https://is2-ssl.mzstatic.com/image/thumb/Music128/v4/9c/5a/94/9c5a94ad-b20b-aaf1-9057-e1fa801ed56a/source/60x60bb.jpg", "artworkUrl100":"https://is2-ssl.mzstatic.com/image/thumb/Music128/v4/9c/5a/94/9c5a94ad-b20b-aaf1-9057-e1fa801ed56a/source/100x100bb.jpg", "collectionPrice":14.99, "trackPrice":1.29, "releaseDate":"2017-09-22T12:00:00Z", "collectionExplicitness":"explicit", "trackExplicitness":"notExplicit", "discCount":1, "discNumber":1, "trackCount":22, "trackNumber":20, "trackTimeMillis":230486, "country":"USA", "currency":"USD", "primaryGenreName":"R&B/Soul", "isStreamable":true}
+song_array = [{"trackName": "Frequency", 
+            "artistName": "Jhené Aiko", 
+            "collectionName": "Trip", 
+            "primaryGenreName": "R&B/Soul",
+            "artworkUrl100": "https://is2-ssl.mzstatic.com/image/thumb/Music128/v4/9c/5a/94/9c5a94ad-b20b-aaf1-9057-e1fa801ed56a/source/100x100bb.jpg",
+            "trackViewUrl": "https://music.apple.com/us/album/frequency/1440907359?i=1440908473&uo=4", 
+            "previewUrl": "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview118/v4/a7/2c/a7/a72ca761-d36f-d383-b62c-43f31a6eb7de/mzaf_3908667139953994176.plus.aac.p.m4a"
+            }]
+song_array.each do |song|
+    Song.create(song)
+end
