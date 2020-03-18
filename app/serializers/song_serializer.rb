24a3d44f8a0341b389e4f36ae932327946ca14ee
Stefanie <stefcodes@gmail.com>
2020-03-17 22:52:41 -0700

    add relationship to song serializer

--- app/serializers/song_serializer.rb
+++ app/serializers/song_serializer.rb
@@ -1,3 +1,5 @@
 class SongSerializer < ActiveModel::Serializer
   attributes :id, :trackName, :artistName, :collectionName, :primaryGenreName, :artworkUrl100, :trackViewUrl, :previewUrl 
+
+  has_many :journals
 end
